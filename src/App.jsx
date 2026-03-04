cf421eb32f0fb8a5fff3b4bc75c721f3f2f67f01
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-03-04 17:17:45 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit CHris chris<code>src/App.jsx</code> and save to reload.
+          Edit Chris chris<code>src/App.jsx</code> and save to reload.
         </p>
         <a
           className="App-link"
